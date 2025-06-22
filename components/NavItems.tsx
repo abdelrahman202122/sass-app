e74216ed28f0add40339fb7a6172468801294703
Abdelrahman <abdoezzat098@gmail.com>
2025-06-22 19:08:21 +0300

    adding navbar items

---
+++ components/NavItems.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+import path from 'path';
+
+const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Companions', href: '/companions' },
+  { label: 'My Journey', href: '/my-journey' },
+];
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <a
+          key={href}
+          href={href}
+          className={cn(pathname === href && 'text-primary font-semibold')}
+        >
+          {label}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
